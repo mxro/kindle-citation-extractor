e1d06aeb3663aaaa9b2c277cf2121ef019ecbedb
Max Rohde <maxrohde.public@gmail.com>
2020-02-02 19:28:10 +1100

    Fix issues around timezones in data parsing

--- test/parse.test.js
+++ test/parse.test.js
@@ -12,7 +12,7 @@ describe('Test parser', () => {
         expect(data[0].quote).toBe('Binof’s talking differently, Damaya realizes suddenly: her words evenly spaced and voice steady, her manner not so much haughty as grave. As if the world’s fate depends upon her finding the answer to her question. As if she isn’t just some spoiled girl from a powerful family who decided on a whim to do something incredibly stupid.');
         expect(data[0].page).toBe('page 271');
         expect(data[0].location).toBe('Location 4144-4146');
-        expect(data[0].dateAdded.toString()).toBe(new Date(Date.parse('2015-12-16T12:57:19.000Z')).toString());
+        expect(data[0].dateAdded.toString()).toBe(new Date(Date.parse('Wednesday, December 16, 2015 11:57:19 PM')).toString());
         
 
         expect(data[1].author).toBe('Sven Beckert');
@@ -20,7 +20,7 @@ describe('Test parser', () => {
         expect(data[1].quote).toBe('The very particular organization of trade, production, and consumption they created exploded the disparate worlds of cotton that had existed for millennia. They animated cotton, invested it with world-changing energy, and then used it as a lever to transform the world.');
         expect(data[1].page).toBe('page 7');
         expect(data[1].location).toBe('Location 95-97');
-        expect(data[1].dateAdded.toString()).toBe(new Date(Date.parse('2015-12-19T04:20:11.000Z')).toString());
+        expect(data[1].dateAdded.toString()).toBe(new Date(Date.parse('Saturday, December 19, 2015 3:20:11 PM')).toString());
         
 
         expect(data[data.length-1].quote).toBe('Three days later, Burke rose in Parliament to give his famous but futile “On Conciliation with America” speech. “A great empire and little minds go ill together,” he proclaimed.');
